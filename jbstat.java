e3e3b02a3bcb077c2f0328c47f14a28da76f5275
Jeff MAURY <jmaury@redhat.com>
2021-06-09 16:49:46 +0200

    feat: enhance jbstat
    - default args
    - skip non release channels
    - multiple plugins support
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- jbstat.java
+++ jbstat.java
@@ -11,18 +11,40 @@ import org.jetbrains.intellij.pluginRepository.model.UpdateBean;
 import java.util.List;
 
 public class jbstat {
+	private static final String[] DEFAULT_ARGS = {
+		"org.jboss.tools.intellij.openshift",
+		"org.jboss.tools.intellij.analytics",
+		"com.redhat.devtools.intellij.quarkus",
+		"com.redhat.devtools.intellij.tekton",
+		"com.redhat.devtools.intellij.knative",
+		"com.redhat.devtools.intellij.kubernetes",
+		"com.redhat.devtools.intellij.telemetry",
+		"com.redhat.devtools.intellij.rsp"
+	};
 
     public static void main(String... args) {
         PluginRepository instance = PluginRepositoryFactory.create();
-        PluginBean plugin = instance.getPluginManager().getPluginByXmlId(args[0],
-        ProductFamily.INTELLIJ);
+		if (args.length == 0) {
+			args = DEFAULT_ARGS;
+		}
+		for(String id : args) {
+			dump(id, instance);
+		}
+    }
+	
+    public static void dump(String id, PluginRepository instance) {
+        PluginBean plugin = instance.getPluginManager().getPluginByXmlId(id, ProductFamily.INTELLIJ);
         List<UpdateBean> versions = instance.getPluginManager().getPluginVersions(plugin.getId());
         for(UpdateBean version : versions) {
             IntellijUpdateMetadata data = instance.getPluginUpdateManager().getIntellijUpdateMetadata(version.getPluginId(), version.getId());
             try {
                 PluginUpdateBean update = instance.getPluginUpdateManager().getUpdateById(data.getId());
-                System.out.println(args[0] + ";" + update.getVersion() + ";" + update.getDownloads());
-            } catch (Exception e) {}
+				if (update.getChannel() == null || update.getChannel().length() == 0) {
+					System.out.println(id + ";" + update.getVersion() + ";" + update.getDownloads());
+				}
+            } catch (Exception e) {
+		    e.printStackTrace();
+	    }
         }
-    }
+    }	
 }
